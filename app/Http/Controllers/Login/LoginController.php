ce5879cf50b717a1e5741dda0ee01e011ef128d1
bai <2829765303@qq.com>
2020-10-06 10:04:50 +0800

    n

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -18,63 +18,15 @@ class LoginController extends Controller
 
     public function register(){
 
-
-		 // Download：https://github.com/aliyun/openapi-sdk-php
-		// Usage：https://github.com/aliyun/openapi-sdk-php/blob/master/README.md
     	return view('/register');
     }
 
-    public function getcode(){
-
-    	$mobile = request()->mobile;
-    	$code = rand(10000,99999);
-    	$res = $this->sendSms($mobile,$code);
-    	 if($res['Code']=='OK'){
-    	    return json_encode(['code'=>'0','msg'=>'发送成功']);
-        }else{
-            return json_encode(['code'=>'2','msg'=>'发送失败']);
-        }
-}
-
-      public function sendSms($mobile,$code)
-    {
-
-AlibabaCloud::accessKeyClient('LTAI4G7geaV9jn6u1buFRqhd', '0Isn5WxtkxK9X7pD5Za4vrDeGZ9P1d')
-                        ->regionId('cn-hangzhou')
-                        ->asDefaultClient();
-
-try {
-    $result = AlibabaCloud::rpc()
-                          ->product('Dysmsapi')
-                          // ->scheme('https') // https | http
-                          ->version('2017-05-25')
-                          ->action('SendSms')
-                          ->method('POST')
-                          ->host('dysmsapi.aliyuncs.com')
-                          ->options([
-                                        'query' => [
-                                          'RegionId' => "cn-hangzhou",
-                                          'PhoneNumbers' => "$mobile",
-                                          'SignName' => "乐柠",
-                                          'TemplateCode' => "SMS_181865178",
-                                          'TemplateParam' => "{code:$code}",
-                                        ],
-                                    ])
-                          ->request();
-    return ($result->toArray());
-} catch (ClientException $e) {
-    return $e->getErrorMessage() . PHP_EOL;
-} catch (ServerException $e) {
-    return $e->getErrorMessage() . PHP_EOL;
-}
-
-    }
-
-    public function store(Request $request){
+    public function registerdo(Request $request){
        $admin_name = $request->post('admin_name');
         $admin_pwd = $request->post('admin_pwd');
         $admin_pwds = $request->post('admin_pwds');
         $admin_tel = $request->post('admin_tel');
+        $admin_email = $request->post('admin_email');
         $len = strlen($admin_pwd);
         $t = Login::where(['admin_tel'=>$admin_tel])->first();
         $a = Login::where(['admin_name'=>$admin_name])->first();
@@ -94,9 +46,13 @@ try {
         $data = [
             'admin_name' => $admin_name,
             'admin_tel' => $admin_tel,
-            'admin_pwd'=>$admin_pwd
+            'admin_pwd'=>$admin_pwd,
+            'admin_email'=>$admin_email,
+            'time'=>time()
+        
         ];
         $res = Login::insert($data);
+        // dd($res);
         if(!$res){
             return json_encode(['code'=>'00005','msg'=>'注册失败']);
         }else{
@@ -104,10 +60,11 @@ try {
         }
     }
 
-    public function create(Request $request){
+    public function logindo(Request $request){
         
         $admin_tel = $request->post('admin_tel');
         $admin_pwd = $request->post('admin_pwd');
+        $time_goods = $request->post('time_goods');
         $u = Login::where(['admin_tel'=>$admin_tel])->first();
         if(!$u){
             return json_encode(['code'=>'00002','msg'=>'账号错误']);
@@ -121,6 +78,10 @@ try {
                 session(['admin_id' => $u['admin_id']]);
                 session(['admin_name' => $u['admin_name']]);
                 $request->session()->save();
+                $where=[
+                    ["admin_id","=",$u['admin_id']]
+                ];
+                Login::where($where)->update(['time_goods'=>time()]);
                 return json_encode(['code'=>'00000','msg'=>'登录成功']);
             }
         }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,17 +12,10 @@
     <link rel="stylesheet" type="text/css" href="/static/css/pages-register.css" />
 </head>
 
-<body>
-	<div class="register py-container ">
 		<!--head-->
-		<div class="logoArea">
-			<a href="" class="logo"></a>
-		</div>
+	<h1>注册</h1>
 		<!--register-->
-		<div class="registerArea">
-			<h3>注册新用户<span class="go">我有账号，去<a href="login" target="_blank">登陆</a></span></h3>
-			<div class="info">
-				<form class="sui-form form-horizontal" action="{{url('/login/store')}}" method="POST">
+				<form  action="{{url('/login/store')}}" method="POST">
 					<div class="control-group">
 						<label class="control-label">用户名：</label>
 						<div class="controls">
@@ -31,12 +24,13 @@
 						</div>
 					</div>
 					<div class="control-group">
-						<label for="inputPassword" class="control-label">登录密码：</label>
+						<label for="inputPassword" class="control-label">密码：</label>
 						<div class="controls">
 							<input type="password" placeholder="设置登录密码" name="admin_pwd" class="input-xfat input-xlarge">
 							   <div><span class="pass2"></span></div>
 						</div>
 					</div>
+			
 					<div class="control-group">
 						<label for="inputPassword" class="control-label">确认密码：</label>
 						<div class="controls">
@@ -46,51 +40,28 @@
 					</div>
 					
 					<div class="control-group">
+						<label class="control-label">邮箱</label>
+						<div class="controls">
+							<input type="text" placeholder="邮箱"  name="admin_email" class="input-xfat input-xlarge">
+							   <div><span class="pass1"></span></div>
+						</div>
+					</div>
+
+					<div class="control-group">
 						<label class="control-label">手机号：</label>
 						<div class="controls">
 							<input type="text" placeholder="请输入你的手机号" name="admin_tel" class="input-xfat input-xlarge">
 						</div>
-					</div>
-					<div class="control-group">
-						<label for="inputPassword" class="control-label">短信验证码：</label>
-						<div class="controls">
-							<input type="text" placeholder="短信验证码" name="code" class="input-xfat input-xlarge">  <a href="javascript:void(0)" id="getcode">获取短信验证码</a>
-						</div>
-					</div>
-					
-					<div class="control-group">
-						<label for="inputPassword" class="control-label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-						<div class="controls">
-							<input name="m1" type="checkbox" value="2" checked=""><span>同意协议并注册《品优购用户协议》</span>
-						</div>
-					</div>
+					</div>	
 					<div class="control-group">
 						<label class="control-label"></label>
 						<div class="controls btn-reg">
-							<imput class="sui-btn btn-block btn-xlarge btn-danger"  id="btn-s" type="button" target="_blank">完成注册
+							<imput class=" btn-danger"  id="btn-s" type="button" target="_blank">完成注册
 						</div>
 					</div>
 				</form>
 				<div class="clearfix"></div>
-			</div>
-		</div>
 		<!--foot-->
-		<div class="py-container copyright">
-			<ul>
-				<li>关于我们</li>
-				<li>联系我们</li>
-				<li>联系客服</li>
-				<li>商家入驻</li>
-				<li>营销中心</li>
-				<li>手机品优购</li>
-				<li>销售联盟</li>
-				<li>品优购社区</li>
-			</ul>
-			<div class="address">地址：北京市昌平区建材城西路金燕龙办公楼一层 邮编：100096 电话：400-618-4000 传真：010-82935100</div>
-			<div class="beian">京ICP备08001421号京公网安备110108007702
-			</div>
-		</div>
-	</div>
 
 
 
@@ -99,7 +70,6 @@
 <script type="text/javascript" src="/static/js/plugins/sui/sui.min.js"></script>
 <script type="text/javascript" src="/static/js/plugins/jquery-placeholder/jquery.placeholder.min.js"></script>
 <script type="text/javascript" src="/static/js/pages/register.js"></script>
-</body>
 <script type="text/javascript">
 	   $(document).on('blur','input[name="admin_name"]',function(){
         var _this = $(this);
@@ -150,25 +120,12 @@
         $(document).on('click','.btn-danger',function(){
         	
         	var admin_name = $('input[name="admin_name"]').val();
-        	
         	var admin_pwd = $('input[name="admin_pwd"]').val();
         	var admin_pwds = $('input[name="admin_pwds"]').val();
         	var admin_tel = $('input[name="admin_tel"]').val();
-        	var code = $('input[name="code"]').val();
-        	 $.post('/login/store',{admin_name:admin_name,admin_pwd:admin_pwd,admin_pwds:admin_pwds,admin_tel:admin_tel,code:code},function (result) {
+			var admin_email = $('input[name="admin_email"]').val();
+        	$.post('/login/registerdo',{admin_name:admin_name,admin_pwd:admin_pwd,admin_pwds:admin_pwds,admin_tel:admin_tel,admin_email:admin_email},function (result) {
             if(result.code=='00001'){
-                alert(result.msg);
-            }
-            if(result.code=='00002'){
-                alert(result.msg);
-            }
-            if(result.code=='00003'){
-                alert(result.msg);
-            }
-            if(result.code=='00004'){
-                alert(result.msg);
-            }
-            if(result.code=='00005'){
                 alert(result.msg);
             }
             if(result.code=='00000'){
